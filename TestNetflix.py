c1235df405034b31260a997e3935a36f9ee1a7c6
Carlos A. Rodriguez <car3564@utexas.edu>
2014-06-24 17:56:47 -0500

    Made the first half of the unit tests

---
+++ TestNetflix.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+"""
+To test the program:
+    % coverage3 run --branch TestNetflix.py
+
+To obtain coverage of the test:
+    % coverage3 report -m
+"""
+
+# -------
+# imports
+# -------
+
+from io       import StringIO
+from unittest import main, TestCase
+
+from Netflix import netflix_eval, rmse_map_sum, sqre_diff, netflix_solve, netflix_write
+
+# -----------
+# TestNetflix
+# -----------
+
+class TestNetflix (TestCase) :
+    # ----
+    # read
+    # ----
+
+    def test_rmse_1 (self) :
+        a = [1, 2, 1]
+        p = [2, 1, 2]
+        r = rmse_map_sum(a, p)
+        self.assertEqual(r, 1)
+
+    def test_rmse_2 (self):
+        a = [2, 3, 4]
+        p = [4, 5, 6]
+        r = rmse_map_sum(a, p)
+        self.assertEqual(r, 2)
+
+    def test_rmse_3 (self):
+        a = [1, 3, 4]
+        p = [3, 1.5, 1]
+        r = rmse_map_sum(a, p)
+        self.assertEqual(r, 2.2546)
+
+
+    # ---------
+    # sqre_diff
+    # ---------
+
+    def test_sqdiff_1 (self) :
+        v = sqre_diff(4, 2)
+        self.assertEqual(v, 4)
+
+    def test_sqdiff_2 (self) :
+        v = sqre_diff(20, 10)
+        self.assertEqual(v, 100)
+
+    def test_sqdiff_3 (self) :
+        v = sqre_diff(0, 10)
+        self.assertEqual(v, 100)
+
+
+    # -----
+    # write
+    # -----
+
+    def test_write_1 (self) :
+        w = StringIO()
+        netflix_write(w, 'm', 10851)
+        self.assertEqual(w.getvalue(), "10851:\n")
+
+    def test_write_2 (self) :
+        w = StringIO()
+        netflix_write(w, 'p', 3.56)
+        self.assertEqual(w.getvalue(), "3.56\n")
+
+    def test_write_3 (self) :
+        w = StringIO()
+        netflix_write(w, 'r', 1.0031)
+        self.assertEqual(w.getvalue(), "RMSE: 1.0031\n")
+
+    # -----
+    # solve
+    # -----
+
+    # def test_solve_1 (self) :
+    #     r = StringIO("1:\n30878\n2647871\n1283744\n")
+    #     w = StringIO()
+    #     netflix_solve(r, w)
+    #     self.assertEqual(w.getvalue(), )
+
+    # def test_solve_2 (self) :
+    #     r = StringIO("10:\n1952305\n1531863\n")
+    #     w = StringIO()
+    #     collatz_solve(r, w)
+    #     self.assertEqual(w.getvalue(), )
+
+    # def test_solve_3 (self) :
+    #     r = StringIO("1000:\n2326571\n977808\n1010534\n1861759")
+    #     w = StringIO()
+    #     collatz_solve(r, w)
+    #     self.assertEqual(w.getvalue(), )
+
+
+    # ----
+    # eval
+    # ----
+
+    # def test_eval_1 (self) :
+        
+
+    # def test_eval_2 (self) :
+        
+
+    # def test_eval_3 (self) :
+        
+
+# ----
+# main
+# ----
+
+main()
+

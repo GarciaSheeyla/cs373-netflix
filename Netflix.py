54fc6783ccd2d555cca9775c069bcb619b6eac40
Sheeyla Garcia <sayla2691@utexas.edu>
2014-06-26 15:31:25 -0500

    Created Netflix.html

--- Netflix.py
+++ Netflix.py
@@ -3,7 +3,7 @@ import sys
 import json
 import math
 from functools import reduce
-import fileinput
+
 # ----------------
 # global variables
 # -------------
@@ -13,9 +13,6 @@ customersDict = {}
 with open(r'/u/mukund/netflix-tests/bryan-customer_cache.json', 'r') as file:
   customersDict = json.load(file)
 
-
-
-
 #Cache holds information about each movie. The keys are the movieIDs and the value is a 3 element list that holds
 #The average movie rating, standard deviation and the number of ratings given
 movieDict  = {}
@@ -24,9 +21,7 @@ with open(r'/u/mukund/netflix-tests/frankc-movie_cache.json','r') as file:
 
 
 
-#Cache hold the actual ratings for customers in probe.txt
-#  cAnswerProbe = open(r'/u/mukund/netflix-tests/osl62-AnswerCache.json','r')
-
+#Cache hold the actual ratings for customers in probe.txt\
 cAnswerProbeDict = {}
 with open(r'/u/mukund/netflix-tests/osl62-AnswerCache.json','r') as file:
   cAnswerProbeDict = json.load(file)
@@ -36,8 +31,14 @@ with open(r'/u/mukund/netflix-tests/osl62-AnswerCache.json','r') as file:
 # ------------
 
 def netflix_eval(mid, cid) :
-  global alist
-
+  ''' 
+  mid        is the movie id
+  cid        is the customers id
+  avgCosRat  is the average rating customers give
+  movieAvg   is the average rating given to the movie
+  prediction is the prediction made based on algorithm
+  returns prediction
+  '''
   assert (int(cid) > 0 and int(cid) <= 2649429)
   assert (int(mid) > 0 and int(mid) <= 17770)
 
@@ -48,9 +49,8 @@ def netflix_eval(mid, cid) :
   stdeviation = avgStdNumRat[1]
   numRat =  avgStdNumRat[2]
 
-  prediction =  round( (((.521 * avgCosRat)  + (.521  * movieAvg) )  - .14) ,2) 
-  # print(actual)
-    
+  prediction =  round( (((.521 * avgCosRat)  + (.521  * movieAvg) )  - .14) ,2)   
+  
   assert (prediction >= 0.0 and prediction <= 5.0)
   return  prediction
 
@@ -80,10 +80,17 @@ def sqre_diff (x, y) :
 # -------------
 
 def netflix_solve(r,w):
+  ''' 
+  r   reader
+  w   writer
+  alist list holds actual values for each movie
+  plist list hold predicted vales for each movie
+  calls netflix_write
+  '''
   alist = []
   plist = []
 
-  for line in r :#fileinput.input():
+  for line in r :
     line = line.rstrip("\n")
     if (line == ""):
       break
@@ -103,11 +110,7 @@ def netflix_solve(r,w):
       assert (actual >= 0 and actual <= 5)
       alist.append(actual)
       netflix_write(w, 'p', prediction)
-  # print ("alist", list(alist))
-  # print ("plist", list(plist))
-  checkit = rmse_map_sum(alist, plist)
-  # print ("checkit is ", checkit)
-  netflix_write(w, 'r', checkit)
+  netflix_write(w, 'r', rmse_map_sum(alist, plist))
   return
 
 
@@ -117,6 +120,15 @@ def netflix_solve(r,w):
 # -------------
 
 def netflix_write(w, cflag, data):
+  '''
+  w writer
+  data 
+  cflag: 
+    m - movie
+    p - prediction
+    r - RMSE
+  prints the data given formated in the desired form based on cflag
+  '''
   assert (cflag == 'm' or cflag == 'p' or cflag == 'r')
   assert (float(data) > 0.0 and float(data) <= 2649429.0)
   
